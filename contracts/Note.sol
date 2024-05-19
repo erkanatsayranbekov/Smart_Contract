342c1f3e85041ea999c33b8ae54fe6f27c1f0a1d
Yerkanat <sayranbekov.0000@gmail.com>
2024-05-19 09:44:15 +0500

    second commit

--- contracts/Note.sol
+++ contracts/Note.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 contract Note {
+    string note;
 
-    string internal note; 
+    function set_note(string memory new_note) public {
+        note = new_note;
+    }
 
     function get_note() public view returns (string memory) {
         return note;
-    }
-
-    function set_note(string calldata _note) public {
-        note = _note;
     }
 }
